42604edd253bf619db88268e5330d143d5cad3cc
YigitCan Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-03-30 23:01:18 +0300

    init

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,9 +1,9 @@
-STATUS_CHOICES = {"paid"=>"Ödendi.", "not_paid"=>"Ödenmedi."}
+STATUS_CHOICES = {"paid"=>"Odendi.", "not_paid"=>"Odenmedi."}
 
 class Credit < ActiveRecord::Base
   belongs_to :friend
   validates_inclusion_of :durum, :in => STATUS_CHOICES.keys
-  validates_numericality_of :miktar, :presence => true, :message => '^Numerik bir değer giriniz.'
+  validates_numericality_of :miktar, :presence => true, :message => '^Numerik bir deger giriniz.'
 
   def get_status
     STATUS_CHOICES[durum]
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,24 +1,24 @@
 # -*- coding:utf-8 -*-
 class Friend < ActiveRecord::Base
   belongs_to  :user
-  has_many  :debits
-  accepts_nested_attributes_for :debits
-  validates_presence_of :name, :message => '^İsim alanı gereklidir.'
-  validates_presence_of :email, :message => '^Email alanı gereklidir.'
+  has_many  :credits
+  accepts_nested_attributes_for :credits
+  validates_presence_of :name, :message => '^isim alani gereklidir.'
+  validates_presence_of :email, :message => '^email alani  gereklidir.'
   validates_format_of     :email,
                           :with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
-                          :message    => '^Geçerli bir email giriniz.'
+                          :message    => '^Gecerli bir email giriniz'
 
   def total_not_paid
-      Debit.sum(:amount, :group=>:friend_id, :conditions=>["status='not_paid'"])[id] or 0
+      credits.sum(:amount, :group=>:friend_id, :conditions=>["status='not_paid'"])[id] or 0
   end
 
   def total_paid
-      Debit.sum(:amount, :group=>:friend_id, :conditions=>["status='paid'"])[id] or 0
+      credits.sum(:amount, :group=>:friend_id, :conditions=>["status='paid'"])[id] or 0
   end
 
   def total_debit
-    self.debits.sum(:amount) or 0
+    self.credits.sum(:amount) or 0
   end
 
   def to_s
