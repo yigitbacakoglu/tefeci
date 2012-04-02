c83247323f10bccb5f36ee42413d3b2bec1bb924
YigitCan Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-04-02 17:08:01 +0300

    email

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -84,7 +84,7 @@ before_filter :login_required
     
     UserMailer.credit_confirmation(@friend).deliver
      respond_to do |format|
-      format.html { redirect_to @friend, :notice => 'ok' }
+      format.html { redirect_to @friend}
       format.json { head :no_content }
     end
     
