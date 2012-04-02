6be37062901d545ff3e1c441db2416bc365d403e
YigitCan Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-04-02 23:47:21 +0300

    bugfix

--- app/controllers/odenen_controller.rb
+++ app/controllers/odenen_controller.rb
@@ -2,8 +2,8 @@ class OdenenController < ApplicationController
  
   before_filter :login_required
   def index  
-
-    @credits=Credit.find_all_by_durum("paid")
+	@friend=Friend.find_by_user_id(@current_user.uid)
+    @credits=Credit.find_all_by_durum_and_friend_id("paid",@friend.id)
 
   end                                                              
  
--- app/controllers/odenmeyen_controller.rb
+++ app/controllers/odenmeyen_controller.rb
@@ -2,7 +2,10 @@ class OdenmeyenController < ApplicationController
  before_filter :login_required
   
   def index  
-  	@credits=Credit.find_all_by_durum("not_paid")
+
+  	# find by durum ve friend id olacak. friend idsini current user uid den alicak. 
+  	@friend=Friend.find_by_user_id(@current_user.uid)
+  	@credits=Credit.find_all_by_durum_and_friend_id("not_paid",@friend.id)
                                                             
   end                                                              
  
