b8b06d20b9e486cfa596847bc0d8eb74dc32680a
YigitCan Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-04-02 16:18:42 +0300

    email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "tefecidefteri@gmail.com"
   def credit_confirmation(friend,user)
   	@friend=friend
-  	@user=User.find_all_by_uid('@friend.user_id')
+  	@user=User.find_all_by_uid("@friend.user_id")
   	@credits=Credit.find_all_by_friend_id(@friend.id)
 
   	mail(:to => friend.email, :subject => "Borcunu Hatirlatirim" )
