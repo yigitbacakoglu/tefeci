7d7274ad60581554b4421cbd95a8eb563129a86b
YigitCan Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-04-05 18:41:14 +0300

    uiAdd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ Tefeci::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-  config.assets.precompile += %w(style.css)
+  
+  config.assets.precompile += %w(layout.css)
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
